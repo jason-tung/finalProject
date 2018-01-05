87a8659e62ed4bbe3fda60c129f238e489e543e8
jason <mickeu420@gmail.com>
2018-01-05 17:03:24 -0500

    filled in methods for empty square and made filled square class

--- EmptySquare.java
+++ EmptySquare.java
@@ -1,3 +1,14 @@
 public class EmptySquare extends Square{
+    
     public Chesspiece getChesspiece(){
-	
+	return null;
+    }
+
+    public boolean hasChesspiece(){
+	return false;
+    }
+
+    public String toString(){
+	return "Square " + who + "\nChesspiece: None";
+    }
+}
