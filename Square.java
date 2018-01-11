239b0a6e8a244d7c7223f3348c95e0aef8390d98
jason <mickeu420@gmail.com>
2018-01-10 23:14:14 -0500

    updated square

--- Square.java
+++ Square.java
@@ -1,15 +1,32 @@
-package squares;
+import java.util.*;
 
 public abstract class Square{
     private int location;
+    private HashMap emptySquaresMap;
 
     public Square(int x){
 	location = x;
+        emptySquaresMap = new HashMap();
+	for (int i = 0; i < 64; i++){
+	    emptySquaresMap.put(i, new EmptySquare());
+	}
+	    
     }
+
 
     public int getLocation(){
 	return location;
     }
+
+    public null makeSquareAt(int location, Chesspiece chesspiece){
+	if (chesspiece == null){
+	    emptySquareMap.get(location);
+	}
+	else{
+	    new FilledSquare(location, chesspiece);
+	}
+    }
+	
 
     abstract Chesspiece getChesspiece();
 
