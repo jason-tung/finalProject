5660ce07d207d796b9a140ce8b953a5510dbccd0
jason <mickeu420@gmail.com>
2018-01-17 04:03:28 -0500

    moved GUI to Frame

---
+++ Frame.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class Frame extends JFrame implements ActionListener {
+
+    private JFrame g;
+    private Dimension gSize;
+    
+  public GUI(){
+      g = new JFrame("CHESS CHESS CHESS CHESS CHESS");
+
+      g.setSize(gSize);
+      g.setVisible(true);
+  }
+}
