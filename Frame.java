7c702667c51d40039acab8edcc247d57fc30c77d
jason <mickeu420@gmail.com>
2018-01-17 05:06:40 -0500

    got working chessbaord pattern

--- Frame.java
+++ Frame.java
@@ -5,26 +5,37 @@ public class Frame extends JFrame{
 
     private JFrame g;
     private JMenuBar navbar;
+    private JPanel board;
+    private int x,y;
     
   public Frame(){
       g = new JFrame("CHESS CHESS CHESS CHESS CHESS");
       navbar = new JMenuBar();
       g.setJMenuBar(navbar);
-      addToBar(navbar);
       g.setSize(new Dimension(600,600));
+      x = 600;
+      y = 600;
       g.setVisible(true);
       g.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+      //cool stuff here
+      addToBar(navbar);
+      makeBoard();
   }
 
     public Frame(int x, int y){
         g = new JFrame("CHESS CHESS CHESS CHESS CHESS");
       navbar = new JMenuBar();
       g.setJMenuBar(navbar);
-      addToBar(navbar);
+      
       g.setSize(new Dimension(x,y));
+      this.x = x;
+      this.y = y;
+      
       g.setVisible(true);
       g.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	
+      addToBar(navbar);
+      makeBoard();
     }
     
 
@@ -36,16 +47,40 @@ public class Frame extends JFrame{
 		public void actionPerformed(ActionEvent e){
 		    System.out.println("testy this is help");
 		}
-	    });
-	    
+	    });	    
 	thingy.add(helpTab);
+	//OPEN BAR HERE
+	JMenuItem openTab = new JMenuItem("Open");
+	openTab.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent e){
+		    System.out.println("testy this opens image");
+		}
+	    });	    
+	thingy.add(openTab);
 	//
-
 
 	//NAVBAR ADD HERE
 	menubar.add(thingy);
-	
-    
-    
     }
+
+    public void makeBoard(){
+	board = new JPanel();
+	board.setLayout(new GridLayout(8,8));
+	int tempy = Math.min(x,y);
+	board.setPreferredSize(new Dimension(tempy,tempy));
+	Color brown = new Color(139,69,19);
+	for (int i = 0; i < 8; i++){
+	    for (int j = 0; j < 8; j++){
+		JPanel temp = new JPanel();
+		temp.setPreferredSize(new Dimension(x/8,y/8));
+		temp.setBackground(Color.white);
+		if ((i+j)%2==0){
+		    temp.setBackground(Color.black);
+		}
+		board.add(temp);
+	    }
+	}
+	g.add(board);
+    }
+    
 }
