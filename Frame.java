2404f4563c8c77f6967d081ea1b6be0d8392f689
jason <mickeu420@gmail.com>
2018-01-17 04:26:45 -0500

    added a JMenuBar to the frame and a tester for it

--- Frame.java
+++ Frame.java
@@ -1,15 +1,51 @@
-import javax.swing.*;
+ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-public class Frame extends JFrame implements ActionListener {
+public class Frame extends JFrame{
 
     private JFrame g;
-    private Dimension gSize;
+    private JMenuBar navbar;
     
-  public GUI(){
+  public Frame(){
       g = new JFrame("CHESS CHESS CHESS CHESS CHESS");
-
-      g.setSize(gSize);
+      navbar = new JMenuBar();
+      g.setJMenuBar(navbar);
+      addToBar(navbar);
+      g.setSize(new Dimension(600,600));
       g.setVisible(true);
+      g.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
   }
+
+    public Frame(int x, int y){
+        g = new JFrame("CHESS CHESS CHESS CHESS CHESS");
+      navbar = new JMenuBar();
+      g.setJMenuBar(navbar);
+      addToBar(navbar);
+      g.setSize(new Dimension(x,y));
+      g.setVisible(true);
+      g.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+    }
+    
+
+    public static void addToBar(JMenuBar menubar){
+        JMenu thingy = new JMenu("Menu");
+	//HELP BAR HERE
+	JMenuItem helpTab = new JMenuItem("Help");
+	helpTab.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent e){
+		    System.out.println("testy this is help");
+		}
+	    });
+	    
+	thingy.add(helpTab);
+	//
+
+
+	//NAVBAR ADD HERE
+	menubar.add(thingy);
+	
+    
+    
+    }
 }
---
+++ Main.java
@@ -0,0 +1,9 @@
+
+public class Main{
+
+    public static void main(String[] args){
+	Board jerry = new Board();
+	System.out.println(jerry);
+	Frame jerry2 = new Frame(1080,1080);
+    }
+}
